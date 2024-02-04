b82d3d4fbb5085bc64e651a9d4a2f09aaad8cc65
donghuck11 <155379984+donghuck11@users.noreply.github.com>
2024-02-04 17:21:19 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int DP[1000000];
+
+int main(void)
+{
+    int x;
+    cin >> x;
+    for(int i=2; i<=x; i++)
+    {
+        DP[i] = DP[i-1] + 1;
+        if(i % 2 == 0 )
+        {
+            DP[i] = min(DP[i], DP[i/2] + 1);
+        }
+        if(i % 3 == 0)
+        {
+            DP[i] = min(DP[i], DP[i/3] + 1);
+        }
+    }
+    cout << DP[x];
+}
