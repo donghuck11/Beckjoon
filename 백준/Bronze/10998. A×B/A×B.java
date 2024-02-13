02c2288cbbde52db66855941d0e9652fc9bf7e9e
donghuck11 <155379984+donghuck11@users.noreply.github.com>
2024-02-13 17:26:12 +0900

    [Bronze V] Title: A×B, Time: 220 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner ac = new Scanner(System.in);
+		int a = ac.nextInt();
+		int b = ac.nextInt();
+		System.out.println(a*b);
+		
+		ac.close();
+
+	}
+
+}
+
