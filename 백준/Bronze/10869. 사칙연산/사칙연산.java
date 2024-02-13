ccc4b02c6c803703632002ddead2493c42d0059c
donghuck11 <155379984+donghuck11@users.noreply.github.com>
2024-02-14 02:21:28 +0900

    [Bronze V] Title: 사칙연산, Time: 204 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner ac = new Scanner(System.in);
+		int a = ac.nextInt();
+		int b = ac.nextInt();
+		
+		ac.close();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+	}
+
+}
