ba9893c0d128707213d794d3464cf54f32910d21
donghuck11 <155379984+donghuck11@users.noreply.github.com>
2024-02-13 17:17:45 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner kc = new Scanner(System.in);
+		int a = kc.nextInt();
+		int b = kc.nextInt();
+		System.out.println(a+b);
+		
+		kc.close();
+
+	}
+
+}
