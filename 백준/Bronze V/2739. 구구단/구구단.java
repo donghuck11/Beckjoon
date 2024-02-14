c4c84d2fdaadb94bb79657c4538a742afe4cd2ba
donghuck11 <155379984+donghuck11@users.noreply.github.com>
2024-02-15 03:10:29 +0900

    [Bronze V] Title: 구구단, Time: 240 ms, Memory: 18492 KB -BaekjoonHub

---
+++ 백준/Bronze V/2739. 구구단/구구단.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner ac = new Scanner(System.in);
+		int N = ac.nextInt();
+		ac.close();
+		for(int i=1; i<=9; i++) {
+			System.out.println(N + " * " + i + " = " + N * i);
+		}
+	}
+
+}
