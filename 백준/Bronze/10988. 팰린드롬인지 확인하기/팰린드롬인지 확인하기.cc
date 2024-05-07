5c6f415db1fd971725941f99a0173efc4bf459be
donghuck11 <155379984+donghuck11@users.noreply.github.com>
2024-05-07 16:28:20 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string a,b;
+    cin >> a;
+    b = a;
+    reverse(a.begin(), a.end());
+    if(a == b)
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << 0;
+    }
+
+    return 0;
+}
