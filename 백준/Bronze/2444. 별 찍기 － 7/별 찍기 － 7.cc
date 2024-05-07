3f8d3aa2019e035fe19ec0173ec3d267f6adc73f
donghuck11 <155379984+donghuck11@users.noreply.github.com>
2024-05-07 16:14:26 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    for(int i=1; i<=num; i++)
+    {
+        for(int j=1; j<=num-i; j++)
+        {
+            cout << " ";
+        }
+        for(int k=1; k<=2*i-1; k++)
+        {
+            cout << "*";
+        }
+        cout << '\n';
+    }
+    for(int i = num - 1; i > 0; i--)
+    {
+        for(int j=0; j<num-i; j++)
+        {
+            cout << " ";
+        }
+        for(int k=1; k<=2*i-1; k++)
+        {
+            cout << "*";
+        }
+        cout << '\n';
+    }
+
+
+    return  0;
+}
